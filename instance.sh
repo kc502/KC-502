a0a49dc83502a36022fdf77513ce5dc8793f23a2
kc502 <145409949+kc502@users.noreply.github.com>
2023-09-21 16:29:57 +0630

    Create instance.sh

---
+++ instance.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+red='\x1b[31;1m'
+yellow='\x1b[33;1m'
+green='\x1b[32;1m'
+plain='\033[0m'
+
+# $1: instance name, $2: machine type, $3: zone, $4: firewall rule name, $5: username, $6: password, $7: message, $8: token
+# if [[ -n $1 ]] && [[ $2 == e2-* ]] && [[ -n $3 ]] && [[ -n $4 ]] && [[ -n $8 ]] && [[ $(($(date +%s) - $8)) -lt 120 ]] && [[ $(($(date +%s) - $8)) -ge 0 ]]; then
+#if [[ -n cluster-404-m ]] && [[ $2 == e2-standard-4 ]] && [[ -n asia-southeast1-b ]] && [[ -n firewall ]] && [[ -n 404 ]] && [[ $(($(date +%s) - 404)) -lt 120 ]] && [[ $(($(date +%s) - 404)) -ge 0 ]]; then
+
+echo -e "${yellow}Creating auth list ...${plain}"
+gcloud auth list
+
+echo -e "${yellow}Creating config list project ...${plain}"
+gcloud config list project
+
+echo -e "${yellow}Creating git clone ...${plain}"
+git clone https://github.com/googlecodelabs/orchestrate-with-kubernetes.git
+
+echo -e "${yellow}Creating Region ...${plain}"
+gcloud config set compute/zone southasia-east1-b
+
+echo -e "${yellow}Creating instance ...${plain}"
+instance=$(gcloud container clusters create bootcamp --image-type "UBUNTU_CONTAINERD" --num-nodes 1 --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw")
+echo -e "${green}Instance created.${plain}"
+
+echo -e "${yellow}Checking firewall rule ...${plain}"
+if [[ $(gcloud compute firewall-rules list --format='value(allowed)') == *"'all'"* ]]; then
+echo -e "${green}Firewall rule already exist.${plain}"
+else
+echo -e "${yellow}Creating firewall rule ...${plain}"
+gcloud compute firewall-rules create firewall --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=all --source-ranges=0.0.0.0/0 --no-user-output-enabled
+echo -e "${green}Firewall rule created.${plain}"
+fi
+
+#echo -e "\n${red}GCP TAIWAN SERVER 🇹🇼 ${plain}\n"
+
+#echo -e "Username: ${green}$5${plain}, Password: ${green}$6${plain}, SSH Host :  ${green}$(grep -oP '(?<=EXTERNAL_IP: ).*' <<<"$instance")${plain}"
+#echo -e "SSH Host :  ${green}$(grep -oP '(?<=EXTERNAL_IP: ).*' <<<"$instance")${plain}"
+echo ""
+echo "------------------------------------"
+printf "  Proudly developed the script by  \n"
+echo "------------------------------------"
+echo ""
+echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+echo -e "${green}  ______       __        ___                                                 ${plain}"
+echo -e "${green} /\  ___\    /'__`\    /'___`\                                               ${plain}"
+echo -e "${green} \ \ \__/   /\ \/\ \  /\_\ /\ \                                              ${plain}"
+echo -e "${green}  \ \___``\ \ \ \ \ \ \/_/// /__                                             ${plain}"
+echo -e "${green}   \/\ \L\ \ \ \ \_\ \   // /_\ \                                            ${plain}"
+echo -e "${green}    \ \____/  \ \____/  /\______/                                            ${plain}"
+echo -e "${green}     \/___/    \/___/   \/_____/                                             ${plain}"                                                            
+echo -e "${red}Contact the developer https://t.me/kochitt for more information                ${plain}"
+echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+
+#else
+#  echo -e "${red}Token is invalid or expired. Contact the developer https://t.me/nkka404 for more information.${plain}"
+#fi
