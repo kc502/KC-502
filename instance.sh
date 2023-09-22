2e7b44bb2cc8baba9a77bdc6f4388f1e4039e945
kc502 <145409949+kc502@users.noreply.github.com>
2023-09-22 19:34:47 +0630

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -7,13 +7,16 @@ plain='\033[0m'
 
 # $1: instance name, $2: machine type, $3: zone, $4: firewall rule name, $5: username, $6: password, $7: message, $8: token
 # if [[ -n $1 ]] && [[ $2 == e2-* ]] && [[ -n $3 ]] && [[ -n $4 ]] && [[ -n $8 ]] && [[ $(($(date +%s) - $8)) -lt 120 ]] && [[ $(($(date +%s) - $8)) -ge 0 ]]; then
-#if [[ -n cluster-404-m ]] && [[ $2 == e2-standard-4 ]] && [[ -n asia-southeast1-b ]] && [[ -n firewall ]] && [[ -n 404 ]] && [[ $(($(date +%s) - 404)) -lt 120 ]] && [[ $(($(date +%s) - 404)) -ge 0 ]]; then
+#if [[ -n cluster-404-m ]] && [[ $2 == e2-standard-4 ]] && [[ -n asia-southeast1-b ]] && [[ -n firewall ]] && [[ -n 502 ]] && [[ $(($(date +%s) - 502)) -lt 120 ]] && [[ $(($(date +%s) - 404)) -ge 0 ]]; then
 
-#echo -e "${yellow}Creating auth list ...${plain}"
-#gcloud auth list
+echo -e "${yellow}Creating auth list ...${plain}"
+gcloud auth list
+
+echo -e "${yellow}gcloud config list project ...${plain}"
+gcloud config list project
 
 echo -e "${yellow}Creating instance ...${plain}"
-instance=$(gcloud dataproc clusters create cluster-502 --enable-component-gateway --region asia-east1 --single-node --master-machine-type n2-standard-2 --master-boot-disk-size 250 --num-master-local-ssds 1 --shielded-secure-boot --image-version 2.0-ubuntu18)
+instance=$(gcloud dataproc clusters create cluster-502 --region asia-east1 --subnet default --master-machine-type e2-standard-2 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type e2-standard-2 --worker-boot-disk-size 500 --image-version 2.1-debian11)
 echo -e "${green}Instance created.${plain}"
 
 echo -e "${yellow}Checking firewall rule ...${plain}"
